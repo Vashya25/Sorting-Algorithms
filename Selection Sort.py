fbf3c1590521b8d7f5d7e84c8b651270d371a038
Vashya25 <varivashya123@gmail.com>
2025-10-18 10:03:04 +0530

    Update Selection Sort.py

--- Selection Sort.py
+++ Selection Sort.py
@@ -3,13 +3,13 @@
 def selection_sort(arr):
     n = len(arr)
     for i in range(n):
-        # Assume the minimum is the first element of the unsorted part
+        # Assume the minimum is the first element of the unsorted part.
         min_index = i
-        # Test against elements after i to find the smallest
+        # Test against elements after i to find the smallest.
         for j in range(i + 1, n):
             if arr[j] < arr[min_index]:
                 min_index = j
-        # Swap the found minimum element with the first element of the unsorted part
+        # Swap the found minimum element with the first element of the unsorted part.
         arr[i], arr[min_index] = arr[min_index], arr[i]
     return arr
 
